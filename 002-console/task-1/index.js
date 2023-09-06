52a0162fba062c61843908f69261f89b2c49f89f
Denis <web_life@inbox.ru>
2023-09-06 23:56:02 +0300

    решение задачи 1 фикс вер 3

--- 002-console/task-1/index.js
+++ 002-console/task-1/index.js
@@ -8,29 +8,18 @@ const value = argv[dateType];
 
 const date = new Date();
 
-const getDate = (dateType) => {
-  if (['year', 'y'].includes(dateType)) return date.getFullYear();
-  if (['month', 'm'].includes(dateType)) return date.getMonth();
-  if (['date', 'd'].includes(dateType)) return date.getDate();
-  return date;
-};
-
-const setDate = (dateType) => {
-  const diff = type === 'add' ? getDate(dateType) + value : getDate(dateType) - value;
-  if (['year', 'y'].includes(dateType)) return date.setFullYear(diff);
-  if (['month', 'm'].includes(dateType)) return date.setMonth(diff);
-  if (['date', 'd'].includes(dateType)) return date.setDate(diff);
-  return 'setDate';
+const methods = () => {
+  if (['year', 'y'].includes(dateType)) return 'FullYear';
+  if (['month', 'm'].includes(dateType)) return 'Month';
+  if (['date', 'd'].includes(dateType)) return 'Date';
+  return '';
 }
 
-if (type === 'current') console.log(getDate(dateType));
+const condition = () => ['year', 'y', 'month', 'm', 'date', 'd'].includes(dateType);
+const getDate = () => condition() ?  date[`get${methods()}`]() : date;
+const diff = () => type === 'add' ? getDate() + value : getDate() - value;
+const setDate = () => condition() ?  date[`set${methods()}`](new Date(diff())) : date;
 
-if (type === 'add') {
-  const newDate = setDate(dateType);
-  console.log(new Date(newDate).toISOString());
-}
-
-if (type === 'sub') {
-  const newDate = setDate(dateType);
-  console.log(new Date(newDate).toISOString());
-}
+if (type === 'current') console.log(getDate());
+if (type === 'add') console.log(new Date(setDate()).toISOString());
+if (type === 'sub') console.log(new Date(setDate()).toISOString());
